3502179681783e5bc5fd2fbec773c90aa4c7e82d
Damien Ayers <damien@omad.net>
2020-03-11 14:01:46 +1100

    Finish fixing NCI Dataset Ingest Script

--- dags/nci_dataset_ingest.py
+++ dags/nci_dataset_ingest.py
@@ -1,3 +1,4 @@
+from textwrap import dedent
 from airflow import DAG
 from airflow.contrib.operators.ssh_operator import SSHOperator
 from airflow.operators.dummy_operator import DummyOperator
@@ -13,6 +14,7 @@ ingest_products = {
     'ls7_nbart_scene': 'ls7_nbart_albers',
     'ls7_pq_scene': 'ls7_pq_albers',
 }
+
 default_args = {
     'owner': 'Damien Ayers',
     'depends_on_past': False,
@@ -54,33 +56,34 @@ with ingest_dag:
         cd {{work_dir}};
     """
 
-    save_tasks_command = COMMON + """
+    save_tasks_command = dedent(COMMON + """
         {% set ingestion_config = '/g/data/v10/public/modules/' + params.module + 
             '/lib/python3.6/site-packages/digitalearthau/config/ingestion/' + params.ing_product + '.yaml' %}
             
         datacube -v ingest --year {{params.year}} --config-file {{ingestion_config}} --save-tasks {{task_file}}
-    """
+    """)
 
-    test_tasks_command = COMMON + """
+    test_tasks_command = dedent(COMMON + """
         datacube -v ingest --allow-product-changes --load-tasks {{task_file}} --dry-run
-    """
+    """)
 
-    qsubbed_ingest_command = COMMON + """
+    qsubbed_ingest_command = dedent(COMMON + """
         {% set distributed_script = '/g/data/v10/public/modules/' + params.module + 
             '/lib/python3.6/site-packages/digitalearthau/run_distributed.sh' %}
-    
-          qsub \
-          -q {{params.queue}} \
-          -W umask=33 \
-          -l wd,walltime=15:00:00 -m abe \
-          -N ing_{{params.ing_product}}_{{params.year}} \
-          -l ncpus=48,mem=190gb \
-          -l storage=gdata/v10+gdata/fk4+gdata/rs0+gdata/if87 \
-          -P {{ params.project }} -o {{ work_dir }} -e {{ work_dir }} \
-          -- {{ distributed_script}} {{ params.module }} --ppn 48 \
-              datacube -v ingest --allow-product-changes --load_tasks {{ task_file }} \
-              --queue-size {{params.queue_size}} --executor distributed DSCHEDULER
-    """
+        {% set distributed_script = '/home/547/lpgs/bin/run_distributed.sh' %}
+
+        qsub \
+        -N ing_{{params.ing_product}}_{{params.year}} \
+        -q {{params.queue}} \
+        -W umask=33 \
+        -l wd,walltime=15:00:00 -m abe \
+        -l ncpus=48,mem=190gb \
+        -l storage=gdata/v10+gdata/fk4+gdata/rs0+gdata/if87 \
+        -P {{ params.project }} -o {{ work_dir }} -e {{ work_dir }} \
+        -- {{ distributed_script}} {{ params.module }} --ppn 48 \
+        datacube -v ingest --allow-product-changes --load-tasks {{ task_file }} \
+        --queue-size {{params.queue_size}} --executor distributed DSCHEDULER
+    """)
 
     completed = DummyOperator(task_id='all_done')
     for ing_product in ingest_products.values():
@@ -103,12 +106,20 @@ with ingest_dag:
             ssh_conn_id='lpgs_gadi',
             command=qsubbed_ingest_command,
             params={'ing_product': ing_product},
+            do_xcom_push=True,
 
         )
         wait_for_completion = PBSJobSensor(
             task_id=f'wait_for_{ing_product}_ingest',
             ssh_conn_id='lpgs_gadi',
-            pbs_job_id=f"{{ ti.xcom_pull(task_ids='{submit_task_id}') }}"
+            pbs_job_id="{{ ti.xcom_pull(task_ids='%s') }}" % submit_task_id
         )
 
         start >> save_tasks >> test_tasks >> submit_ingest_job >> wait_for_completion >> completed
+
+"""
+f = open('tasks.bin', 'rb')
+while pickle.load(f): n += 1                                                                                                                                                                                      │··········
+                                                                                                                                                                                                                          │··········
+EOFError: Ran out of input        
+"""
