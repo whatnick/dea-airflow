4b995409826869185140400275e814247b8743bc
Damien Ayers <damien@omad.net>
2020-05-01 13:11:50 +1000

    Daily re-build of dea/unstable NCI module

--- dags/nci_build_dea_unstable_module.py
+++ dags/nci_build_dea_unstable_module.py
@@ -6,18 +6,26 @@ from airflow import DAG
 from airflow.contrib.operators.ssh_operator import SSHOperator
 from datetime import datetime, timedelta
 
+from airflow.operators.email_operator import EmailOperator
+from airflow.operators.slack_operator import SlackAPIOperator, SlackAPIPostOperator
+
+import pendulum
+
+local_tz = pendulum.timezone("Australia/Canberra")
+
 default_args = {
     'owner': 'dayers',
-    'start_date': datetime(2020, 3, 12),
+    'start_date': datetime(2020, 3, 12, tzinfo=local_tz),
     'retries': 1,
-    'retry_delay': timedelta(minutes=1),
+    'retry_delay': timedelta(minutes=10),
     'timeout': 1200,  # For running SSH Commands
 }
 
 dag = DAG(
     'nci_build_dea_unstable_module',
     default_args=default_args,
-    schedule_interval=None,
+    schedule_interval='@daily',
+    catchup=False,
     tags=['nci'],
 )
 
@@ -37,3 +45,22 @@ with dag:
         ./build_environment_module.py dea_unstable/modulespec.yaml
         """,
     )
+
+    post_to_slack = SlackAPIPostOperator(
+        task_id='post_to_slack',
+        slack_conn_id='',
+        channel='#dea-beginners',
+        username='airflow-bot',
+        text='Successfully built new dea/unstable module on the NCI',
+        icon_url='',
+
+    )
+
+    send_email = EmailOperator(
+        task_id='send_email',
+        to='damien@omad.net',
+        subject='New dea/unstable Module',
+        html_content='Successfully built new dea/unstable module on the NCI',
+    )
+
+    build_env_task >> [post_to_slack, send_email]
